3a5ff7fbc5d89f57c5424e6d6e4afc8c40dad044
Muthu Raman <110217804+Ronaldo-00@users.noreply.github.com>
2023-07-07 12:57:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//text auto type script
 
 const typed = new Typed(".auto-type",{
     strings: ["Improve your Business"],
@@ -6,12 +7,28 @@ const typed = new Typed(".auto-type",{
      loop:true
 })
 
-
-//open the main nav unorder list
+//open the main nav unorder lists
 function opentoggle(){
     document.getElementById("mainNav").style.left="0";
 }
 //close the main nav lists
 function closetoggle(){
     document.getElementById("mainNav").style.left="-100%";
+}
+
+//scroll on top function script
+
+const mybutton = document.getElementById("scroll");
+window.onscroll = function() {scrollings()};
+
+function scrollings() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
